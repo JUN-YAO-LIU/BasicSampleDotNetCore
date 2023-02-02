f5cadf8d67ea11dd0486688ed19ceb3d884d1873
jim.liu <jim.liu@numiner.io>
2023-02-02 18:10:43 +0800

    加上xss

--- BasicSample/Controllers/SecurityController.cs
+++ BasicSample/Controllers/SecurityController.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace BasicSample.Controllers
 {
     public class SecurityController : Controller
     {
+        private readonly HtmlEncoder _htmlEncoder;
+        private readonly JavaScriptEncoder _javaScriptEncoder;
+        private readonly UrlEncoder _urlEncoder;
+
+        public SecurityController(
+            HtmlEncoder htmlEncoder,
+            JavaScriptEncoder javaScriptEncoder,
+            UrlEncoder urlEncoder)
+        {
+            _htmlEncoder = htmlEncoder;
+            _javaScriptEncoder = javaScriptEncoder;
+            _urlEncoder = urlEncoder;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -29,6 +44,28 @@ namespace BasicSample.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult CSRF(string name)
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult XSS()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult XSS(string content)
+        {
+            var htmlContnet = _htmlEncoder.Encode(content);
+            var jsContnet = _javaScriptEncoder.Encode(content);
+            var urlContnet = _urlEncoder.Encode(content);
+
+            ViewBag.html = htmlContnet;
+            ViewBag.dontEncodeHtml = content;
+            ViewBag.js = jsContnet;
+            ViewBag.url = urlContnet;
+
             return View();
         }
     }
