c67aee21133f319211bdfa637b06890203c7e2ed
jim.liu <jim.liu@numiner.io>
2023-02-13 18:24:33 +0800

    d

---
+++ BasicSample/Models/TestDataTypeHtmlHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+
+namespace BasicSample.Models
+{
+    public class TestDataTypeHtmlHelper
+    {
+        public string TestString { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        public string TestPassword { get; set; } = string.Empty;
+
+        [DataType(DataType.EmailAddress)]
+        public string ColName { get; set; }
+
+        [DataType(DataType.Date)]
+        public string ColName1 { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public string ColName2 { get; set; }
+
+        [DataType(DataType.Currency)]
+        public string ColName3 { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string ColName4 { get; set; }
+
+        [DataType(DataType.Url)]
+        public string ColName5 { get; set; }
+    }
+}
