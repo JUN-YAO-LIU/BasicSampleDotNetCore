bb407050f7eb4700379d5c9bb471f080652efc63
jim.liu <jim.liu@numiner.io>
2023-04-14 10:13:54 +0800

    uploadfile

---
+++ BasicSample/Controllers/TagHelperController.cs
@@ -0,0 +1,34 @@
+﻿using BasicSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace BasicSample.Controllers
+{
+    public class TagHelperController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UploadFiles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(UploadData model)
+        {
+            if (model.File is not null)
+            {
+                var file = model.File;
+                string uploadFilePath = $"{Directory.GetCurrentDirectory()}/wwwroot/{file.FileName}";
+
+                using var stream = System.IO.File.Create(uploadFilePath);
+                await file.CopyToAsync(stream);
+            }
+            return View("UploadFiles");
+        }
+    }
+}
